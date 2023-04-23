a2506924742bc2ba3ca032d045d929b98a3ab402
joshshelley <joshuashelley81@gmail.com>
2023-04-23 17:36:07 -0500

    added controller classes

---
+++ app/src/main/java/edu/utsa/cs3443/quizwiz/controller/AnswerButtonController.java
@@ -0,0 +1,10 @@
+package edu.utsa.cs3443.quizwiz.controller;
+
+import android.view.View;
+
+public class AnswerButtonController implements View.OnClickListener {
+    @Override
+    public void onClick(View v) {
+
+    }
+}
