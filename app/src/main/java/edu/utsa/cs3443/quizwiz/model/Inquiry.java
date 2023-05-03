2fad6f06c3c1e2098c49d0ef11bb9f26240c2ba3
joshshelley <joshuashelley81@gmail.com>
2023-05-03 13:38:25 -0500

    added final touches to my task part 2

--- app/src/main/java/edu/utsa/cs3443/quizwiz/model/Inquiry.java
+++ app/src/main/java/edu/utsa/cs3443/quizwiz/model/Inquiry.java
@@ -1,5 +1,5 @@
 /**
- * @author - Josh Shelley (mzk160)
+ * @author - Josh Shelley (mzk160), Ria James (xuv730), Eric Lopardi (eif833), Zachary Sierra (tnb644)
  *
  * This class represents an Inquiry object in the QuizWiz trivia game
  */
@@ -21,70 +21,121 @@ public class Inquiry {
         this.userAnsChoice = -1;
     }
 
+    /**
+     * @return - String description of inquiry
+     */
     public String getInquiryString() {
         return inquiryString;
     }
 
+    /**
+     * @param inquiryString - String to change the selected inquiry
+     */
     public void setInquiryString(String inquiryString) {
         this.inquiryString = inquiryString;
     }
 
+    /**
+     * @return - String description of the first potential answer for the current inquiry
+     */
     public String getFirstAnsChoice() {
         return firstAnsChoice;
     }
 
+    /**
+     * @param firstAnsChoice - String to change the first answer choice for the current inquiry
+     */
     public void setFirstAnsChoice(String firstAnsChoice) {
         this.firstAnsChoice = firstAnsChoice;
     }
 
+    /**
+     * @return - String description for the second potential answer choice for the current inquiry
+     */
     public String getSecondAnsChoice() {
         return secondAnsChoice;
     }
 
+    /**
+     * @param secondAnsChoice - String to change the second answer choice for the current inquiry
+     */
     public void setSecondAnsChoice(String secondAnsChoice) {
         this.secondAnsChoice = secondAnsChoice;
     }
 
+    /**
+     * @return - String description for the third potential answer choice for the current inquiry
+     */
     public String getThirdAnsChoice() {
         return thirdAnsChoice;
     }
 
+    /**
+     * @param thirdAnsChoice - String to change the third answer choice for the current inquiry
+     */
     public void setThirdAnsChoice(String thirdAnsChoice) {
         this.thirdAnsChoice = thirdAnsChoice;
     }
 
+    /**
+     * @return - String description for the fourth potential answer choice for the current inquiry
+     */
     public String getFourthAnsChoice() {
         return fourthAnsChoice;
     }
 
+    /**
+     * @param fourthAnsChoice - String to change the fourth answer choice for the current inquiry
+     */
     public void setFourthAnsChoice(String fourthAnsChoice) {
         this.fourthAnsChoice = fourthAnsChoice;
     }
 
+    /**
+     * @return - integer that corresponds to the correct answer choice for the current inquiry
+     */
     public int getRightAnsChoice() {
         return rightAnsChoice;
     }
 
+    /**
+     * @param rightAnsChoice - integer to change the correct answer choice for the current inquiry
+     */
     public void setRightAnsChoice(int rightAnsChoice) {
         this.rightAnsChoice = rightAnsChoice;
     }
 
+    /**
+     * @return - integer that corresponds to the user's current answer choice for the current inquiry
+     */
     public int getUserAnsChoice() {
         return userAnsChoice;
     }
 
+    /**
+     * @param userAnsChoice - integer to change the user's current answer choice for the current inquiry
+     */
     public void setUserAnsChoice(int userAnsChoice) {
         this.userAnsChoice = userAnsChoice;
     }
 
+    /**
+     * @return - integer that corresponds to the image resource ID for the current inquiry
+     */
     public int getPicResID() {
         return picResID;
     }
 
+    /**
+     * @param picResID - integer to change the image resource ID for the current inquiry
+     */
     public void setPicResID(int picResID) {
         this.picResID = picResID;
     }
 
+    /**
+     * @return - boolean value to determine whether the user's answer is correct
+     */
     public boolean isCorrect() {
         return userAnsChoice == rightAnsChoice;
     }
